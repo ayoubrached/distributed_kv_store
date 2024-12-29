94293a18e664509c91346abeb93f4c838b8ce21f
Ayoub Rached <Ayoub@mac.mynetworksettings.com>
2024-12-29 18:23:08 -0500

    Initial commit

---
+++ build/CMakeFiles/DistributedKVStore.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DistributedKVStore.dir/src/main.cpp.o"
+  "CMakeFiles/DistributedKVStore.dir/src/main.cpp.o.d"
+  "DistributedKVStore"
+  "DistributedKVStore.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DistributedKVStore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
